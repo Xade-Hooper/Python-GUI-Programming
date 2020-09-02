60702509c5ff2d1ad99b6dc824f1e61c33709d26
Xade-Hooper <62272846+Xade-Hooper@users.noreply.github.com>
2020-09-01 22:14:04 -0400

    Work for chapter 2, previous program with a save function for the data calculated. current problems: *numbers inserted by user come up as PY_VAR in data file.

---
+++ Chapter02/HomeWork2.py
@@ -0,0 +1,91 @@
+"""A better Hello World for Tkinter"""
+import tkinter as tk
+from tkinter import ttk
+
+class HelloView(tk.Frame):
+    """A friendly liltle module"""
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+
+        self.number_x = tk.StringVar()
+        self.number_y = tk.StringVar()
+        self.number_string = tk.StringVar()
+        self.ans_string = tk.StringVar()
+        self.number_string.set("Insert two numbers for sum")
+        self.ans_string.set("")
+
+        self.name = tk.StringVar()
+        self.hello_string = tk.StringVar()
+        self.hello_string.set("")
+        self.config(background='orange')
+
+        name_label = ttk.Label(self, text="Name:")
+        name_entry = ttk.Entry(self,textvariable=self.name)
+        ch_button = ttk.Button(self, text="Change", command=self.on_change)
+        hello_label = ttk.Label(self, textvariable=self.hello_string,
+                               font=("TkDefaultFont", 64), wraplength=600)
+        hello_label.config(background='orange')
+        number_x_entry = ttk.Entry(self,textvariable=self.number_x)
+        number_y_entry = ttk.Entry(self,textvariable=self.number_y)
+        number_label = ttk.Label(self, textvariable=self.number_string,
+                               font=("TkDefaultFont", 59), wraplength=600)
+        number_label.config(background='orange')
+        ans_label = ttk.Label(self, textvariable=self.ans_string,
+                               font=("TkDefaultFont", 59), wraplength=600)
+        ans_label.config(background='orange')
+        n_sum_button = ttk.Button(self, text="mathplosion",command=self.math_it)
+        save_button = ttk.Button(self, text = "save data",command = self.save_it)
+
+        #Layout form
+        name_label.grid(row=0, column=0, sticky=tk.W)
+        name_entry.grid(row=0,column=1,sticky=(tk.W + tk.E))
+        ch_button.grid(row=0, column=2, sticky=tk.E)
+        hello_label.grid(row=1, column=0, columnspan=3)
+        number_x_entry.grid(row=4, column=0, sticky=tk.W)
+        number_y_entry.grid(row=5, column=0, sticky=tk.W)
+        number_label.grid(row=4,column=1,columnspan=1)
+        ans_label.grid(row=5,column=1,columnspan=1)
+        n_sum_button.grid(row=6,column=0, sticky=tk.E)
+        save_button.grid(row=6,column=6, sticky=(tk.E + tk.W))
+
+        self.columnconfigure(1, weight=1)
+
+    def on_change(self):
+        if self.name.get().strip():
+            self.hello_string.set("Hello " + self.name.get())
+            self.ans_string.set("testing answer label")
+        else:
+            self.hello_string.set("")
+
+    def math_it(self):
+        if self.number_x.get() != "" :
+            x = int(self.number_x.get())
+            y = int(self.number_y.get())
+            answer = x + y
+            self.ans_string.set("And the answer is = " + str(answer))
+        else:
+            self.ans_string.set("")
+    def save_it(self):
+        with open('data.txt','w') as f:
+            x = int(self.number_x.get())
+            y = int(self.number_y.get())
+            answer = x + y
+            f.write(f'Number {str(self.number_x)} and number {str(self.number_y)} equals {str(answer)}')
+
+
+
+
+class MyApplication(tk.Tk):
+    """Hello World Main Application"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.geometry("900x800")
+        self.resizable(width=False, height=False)
+        HelloView(self).grid(sticky=(tk.E + tk.W + tk.N + tk.S))
+        self.config(background='orange')
+        self.columnconfigure(0,weight=1)
+
+
+if __name__=='__main__':
+    app = MyApplication()
+    app.mainloop()
